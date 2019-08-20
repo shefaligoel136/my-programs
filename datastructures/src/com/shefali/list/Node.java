6e8bd028dc43bbd98e42bca8638c15357e443466
Shefali Goel <goel136shefali@gmail.com>
2019-08-20 14:41:09 +0530

    base classes for Queue and Stack

---
+++ datastructures/src/com/shefali/list/Node.java
@@ -0,0 +1,6 @@
+package com.shefali.list;
+
+public class Node {
+    int data;
+    Node next;
+}
---
+++ datastructures/src/com/shefali/list/QueueNodeOperations.java
@@ -0,0 +1,24 @@
+package com.shefali.list;
+
+public class QueueNodeOperations implements NodeOperations {
+
+    @Override
+    public void push(Node head, int data) {
+
+    }
+
+    @Override
+    public Node pop(Node head) {
+        return null;
+    }
+
+    @Override
+    public void show(Node head) {
+
+    }
+
+    @Override
+    public int length(Node head) {
+        return 0;
+    }
+}
